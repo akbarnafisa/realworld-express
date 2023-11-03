588d933a5ec2e9ba6dbb0f7a402d83fc07008cdd
Akbar Nafisa <akbarnj788@gmail.com>
2023-11-03 08:44:26 +0700

    add login api

--- apps/server-express-prisma-pzn/src/routes/public.ts
+++ apps/server-express-prisma-pzn/src/routes/public.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import { registerController } from '../controller/users';
+import { registerController, loginController } from '../controller/users';
 
 const publicRouter = express.Router();
 publicRouter.post('/api/users', registerController);
+publicRouter.post('/api/users/login', loginController);
+
 
 export { publicRouter };
