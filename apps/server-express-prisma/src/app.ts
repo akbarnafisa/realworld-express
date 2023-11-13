68a73b6c6e59e8313dc7e56e15f5d9466313572c
Akbar Nafisa <akbarnj788@gmail.com>
2023-11-14 06:53:46 +0700

    add article and tag api

--- apps/server-express-prisma/src/app.ts
+++ apps/server-express-prisma/src/app.ts
@@ -36,6 +36,10 @@
 import express from 'express';
 import usersRoute from './routes/api/users';
 import userRoute from './routes/api/user';
+import tagsRouter from './routes/api/tags';
+import articlesRouter from './routes/api/articles';
+
+
 
 import { generalErrorHandler, prismaErrorHandler } from './middleware/errorHandling';
 
@@ -45,6 +49,8 @@ app.use(express.json());
 
 app.use('/api/users', usersRoute);
 app.use('/api/user', userRoute);
+app.use("/api/tags", tagsRouter);
+app.use("/api/article", articlesRouter);
 
 app.get('/', (_req, res) => {
   return res.send('hello world');
