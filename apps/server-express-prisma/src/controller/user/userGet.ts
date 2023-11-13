6f2971ceaeb47d000583177571e85d252db81b35
Akbar Nafisa <akbarnj788@gmail.com>
2023-11-14 06:52:18 +0700

    add try catch in user get api

--- apps/server-express-prisma/src/controller/user/userGet.ts
+++ apps/server-express-prisma/src/controller/user/userGet.ts
@@ -3,21 +3,25 @@ import { Request } from 'express-jwt';
 import { ResponseError, TokenPayload, responseFormat, userViewer } from 'validator';
 import userGetPrisma from '../../utils/db/user/userGetPrisma';
 const userGet = async (req: Request, res: Response, next: NextFunction) => {
-  const auth = req.auth as TokenPayload;
+  try {
+    const auth = req.auth as TokenPayload;
 
-  const originData = await userGetPrisma(auth.id);
-  if (!originData) {
-    next(new ResponseError(404, 'User not found!'));
-    return;
+    const originData = await userGetPrisma(auth.id);
+    if (!originData) {
+      next(new ResponseError(404, 'User not found!'));
+      return;
+    }
+
+    return res.status(200).json(
+      responseFormat({
+        error: null,
+        success: true,
+        data: userViewer(originData),
+      }),
+    );
+  } catch (error) {
+    next(error);
   }
-
-  return res.status(200).json(
-    responseFormat({
-      error: null,
-      success: true,
-      data: userViewer(originData),
-    }),
-  );
 };
 
 export default userGet;
