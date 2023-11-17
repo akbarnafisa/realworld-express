9e458b6f853c3cdf0a2ef7942f075505bc580c68
Akbar Nafisa <akbarnj788@gmail.com>
2023-11-17 15:24:44 +0700

    add current user service

--- apps/server-express-prisma-raw/src/services/user/getCurrentUserService.ts
+++ apps/server-express-prisma-raw/src/services/user/getCurrentUserService.ts
@@ -1,7 +1,25 @@
 import { Request } from 'express-jwt';
+import { ResponseError, TokenPayload, userViewer } from 'validator';
+import pool from '../../app/db';
+import { UserModel } from '../../utils/types';
 
-const getCurrentUserService = (req: Request) => {
-  console.log(req);
+const userGetServiceQuery = 'SELECT * from blog_user WHERE(id = $1 AND 1=1) LIMIT 1 OFFSET 0';
+
+const getCurrentUserService = async (req: Request) => {
+  const auth = req.auth as TokenPayload | undefined;
+
+  if (!auth) {
+    throw new ResponseError(401, 'User unauthenticated!');
+  }
+
+  const getUser = await pool.query(userGetServiceQuery, [auth.id]);
+  const user = getUser?.rows[0] as UserModel;
+
+  if (!user) {
+    throw new ResponseError(404, 'User not found!');
+  }
+
+  return userViewer(user)
 };
 
 export default getCurrentUserService;
