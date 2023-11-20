6ee4c1612860e86f9c9e8c548d6d3d766d30f574
Akbar Nafisa <akbarnj788@gmail.com>
2023-11-20 11:58:35 +0700

    refactor: move update user to utils query

---
+++ apps/server-express-prisma-raw/src/utils/db/users/index.ts
@@ -0,0 +1,3 @@
+export { default as getUserByEmail } from './getUserByEmail';
+export { default as updateUser } from './updateUser';
+
---
+++ apps/server-express-prisma-raw/src/utils/db/users/updateUser.ts
@@ -0,0 +1,22 @@
+import { UserUpdateInputType } from 'validator';
+import pool from '../../../app/db';
+
+const updateUser = async (data: Partial<UserUpdateInputType>, id: number) => {
+  const inputData = Object.keys(data)
+    .map((key, index) => {
+      return `${key} = $${index + 2}`;
+    })
+    .join(', ');
+
+  const query = `UPDATE blog_user SET ${inputData} WHERE (id = $1 AND 1=1) RETURNING *`;
+  const values = [id, ...Object.values(data)];
+
+  console.log({
+    query,
+    values,
+  });
+
+  return await pool.query(query, values);
+};
+
+export default updateUser;
