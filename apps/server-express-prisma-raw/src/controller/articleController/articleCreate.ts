4d0b773679910e954ea6672be643701090721e35
Akbar Nafisa <akbarnj788@gmail.com>
2023-11-20 11:32:53 +0700

    rename article name

--- apps/server-express-prisma-raw/src/controller/articleController/articleCreate.ts
+++ apps/server-express-prisma-raw/src/controller/articleController/articleCreate.ts
@@ -1,10 +1,10 @@
 import type { RequestHandler } from 'express';
 import { responseFormat } from 'validator';
-import { createArticle } from '../../services/articles';
+import { createArticleService } from '../../services/articles';
 
 const articleCreate: RequestHandler = async (req, res, next) => {
   try {
-    const data = await createArticle(req);
+    const data = await createArticleService(req);
     res.status(200).send(
       responseFormat({
         data,
