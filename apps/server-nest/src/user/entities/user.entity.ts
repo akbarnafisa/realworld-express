6726bf81e35cf21751d444cc9f2f3d49221825e9
Akbar Nafisa <akbarnj788@gmail.com>
2023-12-20 15:22:08 +0700

    update user entity

--- apps/server-nest/src/user/entities/user.entity.ts
+++ apps/server-nest/src/user/entities/user.entity.ts
@@ -1,8 +1,15 @@
-export class UserEntity {
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
   id: number;
   bio: string | null;
   email: string;
   image: string | null;
   password: string;
+  username: string;
+}
+
+export class UserArticle implements Pick<User, 'username' | 'image'> {
+  image: string | null;
   username: string;
 }
