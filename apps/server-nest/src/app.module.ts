46001645b6e781a4f5d0c70f8f22bb843cb60df7
Akbar Nafisa <akbarnj788@gmail.com>
2023-12-22 13:52:34 +0700

    init comment API

--- apps/server-nest/src/app.module.ts
+++ apps/server-nest/src/app.module.ts
@@ -4,8 +4,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ArticleModule } from './article/article.module';
 import { TagModule } from './tag/tag.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, AuthModule, ArticleModule, TagModule],
+  imports: [UserModule, PrismaModule, AuthModule, ArticleModule, TagModule, CommentModule],
 })
 export class AppModule {}
