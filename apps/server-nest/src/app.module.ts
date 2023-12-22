95172412257286caaa26849db80158412d99c457
Akbar Nafisa <akbarnj788@gmail.com>
2023-12-23 06:09:28 +0700

    init profile

--- apps/server-nest/src/app.module.ts
+++ apps/server-nest/src/app.module.ts
@@ -5,8 +5,9 @@ import { AuthModule } from './auth/auth.module';
 import { ArticleModule } from './article/article.module';
 import { TagModule } from './tag/tag.module';
 import { CommentModule } from './comment/comment.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, AuthModule, ArticleModule, TagModule, CommentModule],
+  imports: [UserModule, PrismaModule, AuthModule, ArticleModule, TagModule, CommentModule, ProfileModule],
 })
 export class AppModule {}
